5b8565b56126ac559fd8d2edb5af13bbbb30dbef
TOPBOX\Chris <s>
2018-05-13 00:41:58 -0400

    Added login page styling. Updated google-services.json. Modified the build.gradle (app and root level)

---
+++ app/src/main/java/com/wew/azizchr/guidezprototype/AuthActivity.java
@@ -0,0 +1,23 @@
+package com.wew.azizchr.guidezprototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+        //sets the status bar color
+        if (android.os.Build.VERSION.SDK_INT >= 21){
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.statusbarpurple));
+        }
+    }
+}
--- app/src/main/java/com/wew/azizchr/guidezprototype/MainPage.java
+++ app/src/main/java/com/wew/azizchr/guidezprototype/MainPage.java
@@ -21,6 +21,9 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.kbeanie.multipicker.api.CameraImagePicker;
 import com.kbeanie.multipicker.api.ImagePicker;
 import com.kbeanie.multipicker.api.Picker;
@@ -34,6 +37,9 @@ public class MainPage extends AppCompatActivity {
 
     LinearLayout layoutFeed;
     int index;
+
+    //Checks current Auth state
+    //private FirebaseAuth mAuth;
 
     private ImagePicker imgPicker;
     private CameraImagePicker camera;
@@ -55,6 +61,8 @@ public class MainPage extends AppCompatActivity {
             window.setStatusBarColor(this.getResources().getColor(R.color.statusbarpurple));
         }
 
+
+
         layoutFeed = (LinearLayout) findViewById(R.id.layoutFeed);
         index = 0;
         camera = new CameraImagePicker(MainPage.this);
@@ -71,7 +79,18 @@ public class MainPage extends AppCompatActivity {
 
             }
         });
+
+        //init firebase auth instance
+        //mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in
+        //FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
 
     /**
      * Brings up a menu of image options for the user on button press
@@ -131,7 +150,7 @@ public class MainPage extends AppCompatActivity {
             setSpinnerListeners(spinner);
 
             ImageView newImgView = new ImageView(MainPage.this);
-            newImgView.setImageURI(imageUri);
+            Glide.with(this).load(imageUri).into(newImgView);
             newImgView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
